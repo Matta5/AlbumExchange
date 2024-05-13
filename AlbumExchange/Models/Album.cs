1e36cefb035b966c68fcc6ce74e0d58b4a2e1c37
Matta5 <mvdheiligenberg2006@gmail.com>
2024-05-13 13:38:16 +0200

    Add project files.

---
+++ AlbumExchange/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿namespace AlbumExchange.Models
+{
+    public class Album
+    {
+        public int album_id { get; set; }
+        public string album_name { get; set; }
+        public string artist { get; set; }
+        public string genre { get; set; }
+        public string release_date { get; set; }
+        public string album_cover { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ AlbumExchange/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace DatabaseConnectie.Models
+{
+    public class User
+    {
+        public string profile_picture { get; set; } = "default.jpg";
+        public int user_id { get; set; }
+        public string username { get; set; } 
+        public string email { get; set; }
+        public string password { get; set; }
+        public string new_username { get; set; }
+
+    }
+}
